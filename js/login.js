2c84d9d77f31e55d0fa336f3495b217069df6336
arturogilja <48344791+arturogilja@users.noreply.github.com>
2019-11-29 09:30:02 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -9,9 +9,4 @@ loginForm.addEventListener("submit", e => {
 
 async function login() {
   // petición a /auth/login
-  const response = await makeRequest("/api/auth/login", "POST", {
-    email: email.value,
-    password: password.value
-  });
-  console.log(response);
 }
